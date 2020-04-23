4a1d4e36aa5e9717cd902c6600ff28701c5518b0
chenjunlee <chenjunlee@gmail.com>
2020-04-23 06:36:04 -0400

    Check Line 3736-3743 the comments

--- CupCarbon-master/src/cupcarbon/CupCarbonController.java
+++ CupCarbon-master/src/cupcarbon/CupCarbonController.java
@@ -3733,6 +3733,14 @@ public class CupCarbonController implements Initializable {
 				listViewConcernedSensors.getItems().clear();
 				for(SensorNode s: u.getSensorsInsideArea() )
 					listViewConcernedSensors.getItems().add(s.getName());
+				//After set up the user area, add one CloudServer to user in the center of area
+				//add by Chenjun
+				//need fix
+				//double pos1 = (MarkerList.markers.get(0).getLatitude() + MarkerList.markers.get(1).getLatitude())/2;
+				//double pos2 = (MarkerList.markers.get(0).getLongitude() + MarkerList.markers.get(1).getLongitude())/2;
+				//CloudServer userServer = new CloudServer(pos1, pos2, 0, 0, 100, 20, -1);
+				//DeviceList.add(userServer);
+				//u.setUserServer(userServer);
 			}
 			MapLayer.repaint();
 
